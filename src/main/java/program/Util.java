472b47e7626bde13936ee80312d1a5ff510525c1
remagine2 <remagine@naver.com>
2023-03-23 17:55:28 +0900

    0323

--- src/main/java/program/Util.java
+++ src/main/java/program/Util.java
@@ -5,11 +5,11 @@ import java.util.Collections;
 import java.util.Objects;
 
 public class Util {
-    public static <T> Collection checkDataParam(Collection<T> param){
+    public static <T> Collection<T> checkDataParam(Collection<T> param) {
         Collection<T> checkedParam = null;
         Objects.requireNonNull(param, "매개변수가 null입니다.");
 
-        if(param.isEmpty()){
+        if (param.isEmpty()) {
             throw new IllegalArgumentException("매개변수가 비었습니다.");
         }
 
@@ -26,25 +26,23 @@ public class Util {
         return checkedParam;
     }
 
-    private static void isReadOnly(Collection param) throws UnsupportedOperationException{
+    private static <T> void isReadOnly(Collection<T> param) throws UnsupportedOperationException {
         //   return param.getClass().getName().contains("Unmodifiable");
         param.add(null);
     }
 
-    private static boolean isDeepCopied(Collection param) {
+    private static <T> boolean isDeepCopied(Collection<T> param) {
+
         return false;
     }
 
-    private static Collection makeDeepCopied(Collection param){
-
+    private static <T> Collection<T> makeDeepCopied(Collection<T> param) {
 
 
         return param;
     }
 
-    private static Collection makeReadOnly(Collection param){
+    private static <T> Collection<T> makeReadOnly(Collection<T> param) {
         return Collections.unmodifiableCollection(param);
     }
-
-
 }
---
+++ src/main/java/program/deepcopy/example.java
@@ -0,0 +1,53 @@
+package program.deepcopy;
+
+public class example {
+    public static class User {
+        private final String name;
+        private final Car car;
+
+        public User(String name, Car car) {
+            this.name = name;
+            this.car = car;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Car getCar() {
+            return car;
+        }
+    }
+
+    public static class Car {
+        private String name;
+
+        public Car(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        User user = new User("택상", new Car("벤츠"));
+        /*User deepCopiedUser = new User(user.getName(), user.getCar());*/
+        User deepCopiedUser = new User(user.getName() , new Car(user.getCar().getName()));
+
+        user.getCar().setName("산타페");
+
+        System.out.println(user.getCar().getName());
+        System.out.println(deepCopiedUser.getCar().getName());
+
+
+
+
+    }
+}
