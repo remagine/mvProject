34168112de0f1f124f07982da5da41e4863cf101
remagine <remagine@naver.com>
2023-03-25 23:01:23 +0900

    0325

--- src/main/java/program/Util.java
+++ src/main/java/program/Util.java
@@ -1,46 +1,63 @@
 package program;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Util {
-    public static <T> List<T> checkParam(List<T> param) {
+    private Util() {
+    }
 
+    public static <T> List<T> checkParam(List<T> param) {
         Objects.requireNonNull(param, "매개변수가 null입니다.");
 
         if (param.isEmpty()) {
             throw new IllegalArgumentException("매개변수가 비었습니다.");
         }
-        List<T> checkedParam = List.copyOf(param);
-        checkedParam = makeReadOnly(checkedParam);
+        // 요소가 empty인 것을 체크하는 이유는 무엇인가요
+        param.forEach(p -> Objects.requireNonNull(p));
 
+        // 정책에 따라 중복이면 1. 중복을 제거한다. 2. 예외처리한다. 3. 허용한다.
+        param = param.stream().distinct().collect(Collectors.toList());
+
+        // 자료가 깊은 복사가 진행되었는지? 어떻게 체크하는지? 체크하기 보단 깊은 복사또는 방어적 복사를 진행하는 것이 좋아보입니다.
+        List<T> checkedParam = List.copyOf(param);
         return checkedParam;
     }
 
     public static <T> Set<T> checkParam(Set<T> param) {
-
         Objects.requireNonNull(param, "매개변수가 null입니다.");
 
         if (param.isEmpty()) {
             throw new IllegalArgumentException("매개변수가 비었습니다.");
         }
+
+        param.forEach(p -> Objects.requireNonNull(p,"매개변수의 요소가 null입니다."));
+        //set은 중복체크가 필요없다.
+
         Set<T> checkedParam = Set.copyOf(param);
-        checkedParam = makeReadOnly(checkedParam);
 
         return checkedParam;
     }
 
-    /*private static Map checkParam(Map param) {
-        Map checkedParam = Map.copyOf(param);
+    private static <K,V> Map<K,V> checkParam(Map<K,V>  param) {
         Objects.requireNonNull(param, "매개변수가 null입니다.");
 
         if (param.isEmpty()) {
             throw new IllegalArgumentException("매개변수가 비었습니다.");
         }
-        checkedParam = makeDeepCopied(checkedParam);
-        checkedParam = makeReadOnly(checkedParam);
+
+        for (Map.Entry<K, V> entry : param.entrySet()) {
+            Objects.requireNonNull(entry.getKey(), "매개변수의 키가 null입니다.");
+            Objects.requireNonNull(entry.getValue(), "매개변수의 값이 null입니다.");
+        }
+        // map은 중복체크가 필요한가?
+        // key는 중복체크 필요없다
+        // value는 중복체크 필요없어 보인다.
+
+        Map<K,V> checkedParam = Map.copyOf(param);
 
         return checkedParam;
-    }*/
+    }
 
     private static <T> void isReadOnly(Collection<T> param) throws UnsupportedOperationException {
         //   return param.getClass().getName().contains("Unmodifiable");
--- src/main/java/program/cat/Cat.java
+++ src/main/java/program/cat/Cat.java
@@ -23,10 +23,11 @@ public class Cat implements Command {
     }
 
     public static Cat fromPaths(List<String> filePathStrings) {
-        List<Path> newPaths = filePathStrings.stream()
+        List<String> checkedParam = Util.checkParam(filePathStrings);
+        List<Path> newPaths = checkedParam.stream()
                 .filter(str -> Objects.nonNull(str))
                 .map(str -> Paths.get(str))
-                .collect(Collectors.toUnmodifiableList()); // 항상 자료를 읽기전용으로 받는 연습
+                .collect(Collectors.toUnmodifiableList());
         return new Cat(newPaths);
     }
 
@@ -38,33 +39,27 @@ public class Cat implements Command {
 
         //시나리오 코딩은 지양
         // 구조적으로 코딩하는 연습 (역활별로 for if등을 사용
-
-//        for(Path filePath : paths){
-//            if (Files.notExists(filePath)) {
-//                throw new IllegalStateException("파일이 존재하지 않습니다. path = " + filePath);
-//            }
-//        }
         // 시나리오를 구조로 바꿀 수 있는 능력을 길러야 한다.
-
+        // 1. paths -> List<InputStream>으로
         List<InputStream> inputStreams = paths.stream().map(path ->{
             if(Files.notExists(path)){
-                return new ByteArrayInputStream(("파일이 존재하지 않습니다. path = " + path).getBytes(StandardCharsets.UTF_8));
+                return new ByteArrayInputStream(("파일이 존재하지 않습니다. path : " + path).getBytes(StandardCharsets.UTF_8));
             }
 
             try {
                 return Files.newInputStream(path);
             } catch (IOException e) {
-                return new ByteArrayInputStream(("파일이 존재하지 않습니다. path = " + path).getBytes(StandardCharsets.UTF_8));
+                return new ByteArrayInputStream(("파일이 존재하지 않습니다. path : " + path).getBytes(StandardCharsets.UTF_8));
             }
-        }).collect(Collectors.toUnmodifiableList());
-
-        for (InputStream inputStream : inputStreams) {
-            try (inputStream;
-                 BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, BUFFER_SIZE);
-            ) {
-                while (true) {
+        }).collect(Collectors.toList());
+        // 2. List<InputStream> 을 출력
+        inputStreams.forEach(inputStream -> {
+            try(inputStream;
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, BUFFER_SIZE);
+            ){
+                while(true){
                     int len = bufferedInputStream.read(buffer);
-                    if (len == -1) {
+                    if(len == -1) {
                         break;
                     }
                     bufferedOutputStream.write(buffer,0,len);
@@ -72,29 +67,12 @@ public class Cat implements Command {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        });
 
         try {
             bufferedOutputStream.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-       /* for (String fileName : fileNames) {
-            File file = new File(fileName);
-            try (InputStream fileInputStream = new FileInputStream(file);
-                 BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, BUFFER_SIZE);
-            ) {
-                while (true) {
-                    int len = bufferedInputStream.read(buffer);
-                    if (len == -1) {
-                        break;
-                    }
-                    bufferedOutputStream.write(buffer, 0, len);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
     }
 }
